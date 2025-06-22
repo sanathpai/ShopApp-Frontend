f9763c932484ff47cf23e53329cc3f370e384ac7
tahsinsoha <soha97368@gmail.com>
2025-06-22 08:45:11 +0600

    insights working in localhost

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://shoppeappnow.com/api',
+  baseURL: 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
